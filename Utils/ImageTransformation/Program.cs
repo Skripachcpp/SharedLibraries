edf5113cd2d021fd803fc55d6f8d5b1db127c136
skripach.cpp <skripach.cpp@gmail.com>
2018-04-13 12:13:48 +0300

    1240x1240

--- Utils/ImageTransformation/Program.cs
+++ Utils/ImageTransformation/Program.cs
@@ -21,11 +21,13 @@ namespace ImageTransformation
                 DirectoryWt.CreateDirectory("files");
 
             const string preview = "preview_150x150";
+            const string preview1240X1240 = "preview_1240x1240";
             // удаляем старые кадрированные изображения
             foreach (var directory in DirectoryWt.GetDirectories("files", true))
             {
                 var directoryName = Path.GetFileName(directory);
-                if (directoryName == preview)
+                //
+                if (directoryName == preview || directoryName == preview1240X1240)
                 {
                     Report.WriteLine(directory, ConsoleColor.Yellow);
                     DirectoryWt.Delete(directory);
@@ -39,25 +41,22 @@ namespace ImageTransformation
                 var directoryName = Path.GetFileName(directory);
 
                 // пропускаем папки с кадрированными изображениями
-                if (directoryName == preview)
+                if (directoryName == preview || directoryName == preview1240X1240)
                     continue;
 
                 // полиучить оригинал
                 using (var image = Image.FromFile(filePath))
                 // кадрирование изображения
                 using (var img = ImgTransformation.Resize(image, 150, 150))
+                using (var img1240X1240 = ImgTransformation.Resize(image, 1240, 1240))
                 {
-                    DirectoryWt.CreateDirectory(Path.Combine(directory, preview));
-
-                    var imgPath = Path.Combine(directory, preview, fileName + ".png");
-                    FileWt.Delete(imgPath);
-                    img.Save(imgPath, ImageFormat.Png);
-                    Report.WriteLine(imgPath, ConsoleColor.Blue);
+                    ImgSave(img, directory, preview, fileName);
+                    ImgSave(img1240X1240, directory, preview1240X1240, fileName);
 
                     var fileExt = Path.GetExtension(filePath);
                     // если формат картинки не png
                     if (fileExt != ".png")
-                    {   
+                    {
                         // сохранить картинку в формате png
                         image.Save(Path.Combine(directory, fileName + ".png"), ImageFormat.Png);
                         // удалить оригинал
@@ -71,6 +70,16 @@ namespace ImageTransformation
             Report.WriteLine();
             Report.WriteLine("готово");
             Console.ReadKey();
+        }
+
+        public static void ImgSave(Image img, string directory, string preview, string fileName)
+        {
+            DirectoryWt.CreateDirectory(Path.Combine(directory, preview));
+
+            var imgPath = Path.Combine(directory, preview, fileName + ".png");
+            FileWt.Delete(imgPath);
+            img.Save(imgPath, ImageFormat.Png);
+            Report.WriteLine(imgPath, ConsoleColor.Blue);
         }
     }
 }
